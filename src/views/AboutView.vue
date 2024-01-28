971866ccd7aaef140619ede930aee52b7e7e6ae3
ryu820 <rhju820@naver.com>
2024-01-28 21:49:21 +0900

    [FEAT]table add #4

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -2,8 +2,19 @@
   <div class="about">
     <h1>This is an about page</h1>
     <h1>존나 어렵다</h1>
-    
+    <TableVue />
   </div>
 </template>
+<script>
+import TableVue from '@/components/TableVue.vue';
+
+export default ({
+  name: "AboutView",
+
+  components: {
+    TableVue,
+  },
+});
+</script>
 
 
