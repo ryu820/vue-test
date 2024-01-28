d3dac768489f88b509c634c6f4d54e1e9b0583cc
ryu820 <rhju820@naver.com>
2024-01-29 07:29:21 +0900

    [FIX]table component 생성 #4

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -2,7 +2,7 @@
   <div class="about">
     <h1>This is an about page</h1>
     <h1>존나 어렵다</h1>
-    <TableVue />
+    <TableVue :headers="customHeaders" :desserts="customDesserts"></TableVue>
   </div>
 </template>
 <script>
@@ -14,6 +14,52 @@ export default ({
   components: {
     TableVue,
   },
+  data() {
+    return {
+      customHeaders: [
+        { align: "start", key: "name", sortable: false, title: "테스트해보자" },
+        { key: "id", title: "Custom id" },
+        { key: "option1", title: "Custom option1" },
+        { key: "option2", title: "Custom option2" },
+        { key: "option3", title: "Custom option3" },
+        { key: "option4", title: "Custom option4" },
+      ],
+      customDesserts: [
+      {
+          name: "Frozen Yogurt1111",
+          id: 159,
+          option1: 6.0,
+          option2: 24,
+          option3: 4.0,
+          option4: 1,
+        },
+        {
+          name: "Ice cream sandwich1111",
+          id: 237,
+          option1: 9.0,
+          option2: 37,
+          option3: 4.3,
+          option4: 1,
+        },
+        {
+          name: "Eclair1111",
+          id: 262,
+          option1: 16.0,
+          option2: 23,
+          option3: 6.0,
+          option4: 7,
+        },
+        {
+          name: "Cupcake11111",
+          id: 305,
+          option1: 3.7,
+          option2: 67,
+          option3: 4.3,
+          option4: 8,
+        },
+      ]
+    }
+  }
 });
 </script>
 
