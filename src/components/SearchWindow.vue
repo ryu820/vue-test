eacddd262f2ce0269c51d068853a479b59f5944d
ryu820 <rhju820@naver.com>
2024-01-29 21:58:07 +0900

    [FIX]캘린더 커스텀 #3

--- src/components/SearchWindow.vue
+++ src/components/SearchWindow.vue
@@ -6,19 +6,31 @@
       prepend-icon="mdi-calendar"
       readonly
     ></v-text-field>
-    {{ dates }}
+  </v-col>
+  <v-row>
     <v-date-picker
-      v-model="dates"
-      locale="ko"
-      is-range
+      v-model="dates[0]"
+      locale="ko-KR"
       mode="date"
       class="datetime-picker"
-      is24hr
+      is24hr="false"
+      weekday-format="getDay"
+      month-format="getMonth"
+
     ></v-date-picker>
-  </v-col>
+    <v-date-picker
+      v-model="dates[1]"
+      locale="ko-KR"
+      no-title
+      mode="date"
+      class="datetime-picker"
+      is24hr="false"
+    ></v-date-picker>
+  </v-row>
 </template>
 
 <script>
+import dayjs from "dayjs";
 export default {
   data() {
     return {
@@ -28,14 +40,40 @@ export default {
   computed: {
     dateRangeText() {
       const startDate = this.dates[0]
-        ? this.dates[0].toISOString().split("T")[0]
+        ? dayjs(this.dates[0]).format("YYYY-MM-DD")
         : "";
       const endDate = this.dates[1]
-        ? this.dates[1].toISOString().split("T")[0]
+        ? dayjs(this.dates[1]).format("YYYY-MM-DD")
         : "";
       return startDate && endDate ? startDate + " ~ " + endDate : "";
     },
   },
-  methods: {},
+  methods: {
+    getDay(date) {
+      const daysOfWeek = ["일", "월", "화", "수", "목", "금", "토"];
+      let i = new Date(date).getDay(date);
+      return daysOfWeek[i];
+    },
+    getMonth(date) {
+      const monthName = [
+        "1월",
+        "2월",
+        "3월",
+        "4월",
+        "5월",
+        "6월",
+        "7월",
+        "8월",
+        "9월",
+        "10월",
+        "11월",
+        "12월",
+      ];
+
+      let i = new Date(date).getMonth(date);
+      return monthName[i];
+    },
+  },
 };
 </script>
+<style></style>
